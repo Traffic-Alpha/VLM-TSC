db1148263b2871ba4931d0bb6232120fe9ec7935
wmn7 <18811371255@163.com>
2025-07-08 20:56:20 +0800

    add plot curve

---
+++ rl_tsc/plot_reward_curve.py
@@ -0,0 +1,19 @@
+'''
+Author: WANG Maonan
+Date: 2025-06-30 21:57:14
+LastEditors: WANG Maonan
+Description: Plot Reward Curve
+LastEditTime: 2025-06-30 21:58:18
+'''
+from tshub.utils.plot_reward_curves import plot_reward_curve
+from tshub.utils.get_abs_path import get_abs_path
+path_convert = get_abs_path(__file__)
+
+
+if __name__ == '__main__':
+    log_files = [
+        path_convert(f'./log/{i}.monitor.csv')
+        for i in range(10)
+    ]
+    output_file = path_convert(f'./reward_curve.png')
+    plot_reward_curve(log_files, output_file=output_file, window_size=1, fill_outliers=False)
