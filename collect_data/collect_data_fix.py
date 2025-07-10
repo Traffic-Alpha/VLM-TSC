da2e9e7a06fd374e71e856c24c38c4a612b0878b
wmn7 <18811371255@163.com>
2025-07-10 15:12:09 +0800

    add fix time policy in repo

---
+++ collect_data/collect_data_fix.py
@@ -0,0 +1,134 @@
+'''
+Author: WANG Maonan
+Date: 2025-06-25 16:45:03
+LastEditors: WANG Maonan
+Description: 使用固定配时收集信息
+LastEditTime: 2025-07-10 14:30:07
+'''
+import os
+
+from tshub.utils.get_abs_path import get_abs_path
+from tshub.utils.init_log import set_logger
+from tshub.utils.format_dict import save_str_to_json
+
+from utils.tsc_env3d import TSCEnvironment3D
+from utils.tsc_wrapper import TSCEnvWrapper
+
+from CONFIG import SCENARIO_CONFIGS
+from collect_data.parse_state import TrafficState2DICT # 将环境信息转换为 JSON
+ 
+def convert_rgb_to_bgr(image):
+    # Convert an RGB image to BGR
+    return image[:, :, ::-1]
+
+path_convert = get_abs_path(__file__)
+set_logger(path_convert('./'))
+
+# 读取场景配置
+SCENARIO_IDX = "Hongkong_YMT_NORMAL" # 可视化场景, SouthKorea_Songdo, Hongkong_YMT
+config = SCENARIO_CONFIGS.get(SCENARIO_IDX) # 获取特定场景的配置
+SCENARIO_NAME = config["SCENARIO_NAME"]
+SUMOCFG = config["SUMOCFG"]
+NETFILE = config["NETFILE"]
+JUNCTION_NAME = config["JUNCTION_NAME"]
+NUM_SECONDS = config["NUM_SECONDS"] # 仿真时间
+CENTER_COORDINATES = config["CENTER_COORDINATES"]
+PHASE_NUMBER = config["PHASE_NUMBER"] # 绿灯相位数量
+MOVEMENT_NUMBER = config["MOVEMENT_NUMBER"] # 有效 movement 的数量
+ACCIDENTS = config["ACCIDENTS"] # 定义的事故
+SPECIAL_VEHICLES = config["SPECIAL_VEHICLES"] # 定义特殊车辆
+
+# 初始化场景飞行器位置, 获得俯视角图像
+aircraft_inits = {
+    'a1': {
+        "aircraft_type": "drone",
+        "action_type": "stationary", # 水平移动
+        "position": CENTER_COORDINATES, "speed":3, "heading":(1,1,0), # 初始位置
+        "communication_range":100, 
+        "if_sumo_visualization":True, "img_file":None,
+        "custom_update_cover_radius":None # 使用自定义的计算
+    },
+}
+
+def make_env(
+        tls_id:str, 
+        sumo_cfg:str, net_file:str,
+        scenario_glb_dir:str, 
+        movement_num:int, 
+        num_seconds:int, use_gui:bool,
+        accident_config,
+        special_vehicle_config,
+        aircraft_inits=None,
+        preset:str="1080P", resolution:float=1,
+        base_path:str = None,
+    ):
+    tsc_env = TSCEnvironment3D(
+        sumo_cfg=sumo_cfg,
+        net_file=net_file,
+        scenario_glb_dir=scenario_glb_dir,
+        preset=preset, resolution=resolution,
+        num_seconds=num_seconds,
+        tls_ids=[tls_id],
+        tls_action_type='choose_next_phase',
+        use_gui=use_gui,
+        aircraft_inits=aircraft_inits,
+        accident_config=accident_config,
+        special_vehicle_config=special_vehicle_config
+    )
+    tsc_env = TSCEnvWrapper(
+        tsc_env, tls_id=tls_id, 
+        movement_num=movement_num,
+        base_path=base_path,
+    )
+
+    return tsc_env
+
+if __name__ == '__main__':
+    
+    tls_id = JUNCTION_NAME
+    sumo_cfg = path_convert(f"../exp_networks/{SCENARIO_NAME}/{SUMOCFG}.sumocfg")
+    net_file = path_convert(f"../exp_networks/{SCENARIO_NAME}/env_normal/{NETFILE}.net.xml")
+    scenario_glb_dir = path_convert(f"../exp_networks/{SCENARIO_NAME}/3d_assets/")
+    base_path = base_path = path_convert(f"../exp_dataset/{SCENARIO_NAME}/") # 存储路径
+    
+    # Init Env
+    tsc_env = make_env(
+        tls_id=tls_id,
+        sumo_cfg=sumo_cfg,
+        net_file=net_file,
+        scenario_glb_dir=scenario_glb_dir,
+        movement_num=MOVEMENT_NUMBER,
+        num_seconds=NUM_SECONDS,
+        accident_config=ACCIDENTS,
+        special_vehicle_config=SPECIAL_VEHICLES,
+        use_gui=True,
+        aircraft_inits=aircraft_inits,
+        preset="480P",
+        resolution=1,
+        base_path=base_path,
+    )
+
+    # Interact with Environment
+    dones = False
+    step_idx = 0
+    rl_state, pixel, infos = tsc_env.reset()
+    traffic_state_to_dict = TrafficState2DICT(tls_id, infos) # 特征转换器
+    index = 0
+    REPEAT_NUMBER = 4 # 每个 traffic phase 重复的次数
+
+    while not dones:
+        action = (index // REPEAT_NUMBER) % PHASE_NUMBER
+        print(f"{index}--{action}")
+        states, rewards, truncated, dones, infos = tsc_env.step(action=action)
+        rl_state, camera_data = states['rl_state'], states['pixel']
+        index += 1
+    
+    # 存储全局信息
+    global_infos = {
+        "scenario_name": SCENARIO_NAME,
+        "tls_id": JUNCTION_NAME,
+        "can_perform_action": {**tsc_env.can_perform_action_infos}
+    }
+    save_str_to_json(global_infos, os.path.join(base_path, "global.json"))
+        
+    tsc_env.close()
