94f705fd3c69c5946131772c006410fd6205de60
wmn7 <18811371255@163.com>
2025-07-22 18:47:04 +0800

    add CONFIG with all envs

--- collect_data/CONFIG.py
+++ collect_data/CONFIG.py
@@ -4,10 +4,11 @@ Date: 2025-06-25 16:50:28
 LastEditors: WANG Maonan
 Description: 场景信息 (netrwork+route+event) 三个部分组成一个场景
 -> Note: 需要提前在 route 中定义好对应的车辆类型
-LastEditTime: 2025-07-17 15:54:14
+LastEditTime: 2025-07-22 18:42:35
 '''
 SCENARIO_CONFIGS = {
-    "Beijing_Beishahe_Normal": {
+    # ===> Beijing_Beishahe
+    "Beijing_Beishahe_Test": {
         "SCENARIO_NAME": "Beijing_Beishahe",
         "SUMOCFG": "easy_high_density.sumocfg",
         "NETFILE": "./networks/easy.net.xml",
@@ -31,6 +32,173 @@ SCENARIO_CONFIGS = {
         "SPECIAL_VEHICLES": None
     },
     
+    # ===> Beijing_Changjianglu
+    "Beijing_Changjianglu_Test": {
+        "SCENARIO_NAME": "Beijing_Changjianglu",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 3,
+        "MOVEMENT_NUMBER": 6, 
+        "CENTER_COORDINATES": (933, 175, 100),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:1, 2:0},
+        "ACCIDENTS": None,
+        # ================== 特殊车辆配置 ==================
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Beijing_Gaojiaoyuan
+    "Beijing_Gaojiaoyuan_Test": {
+        "SCENARIO_NAME": "Beijing_Gaojiaoyuan",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (944, 438, 100),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Beijing_Pinganli
+    "Beijing_Pinganli_Test": {
+        "SCENARIO_NAME": "Beijing_Pinganli",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (1435, 960, 100),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Beijing_Yongrunlu
+    "Beijing_Yongrunlu_Test": {
+        "SCENARIO_NAME": "Beijing_Yongrunlu",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (395, 895, 100),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Chengdu_Chenghannanlu
+    "Chengdu_Chenghannanlu_Test": {
+        "SCENARIO_NAME": "Chengdu_Chenghannanlu",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 3,
+        "MOVEMENT_NUMBER": 6, 
+        "CENTER_COORDINATES": (420, 62, 50),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:1, 2:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Chengdu_Guanghua
+    "Chengdu_Guanghua_Test": {
+        "SCENARIO_NAME": "Chengdu_Guanghua",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (488, 322, 50),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+    
+    # ===> Tianjin_zhijingdao
+    "Tianjin_zhijingdao_Test": {
+        "SCENARIO_NAME": "Tianjin_zhijingdao",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (1733, 1170, 50),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None,
+    },
+
+    # ===> Beijing Beihuan
+    "Beijing_Beihuan_Test": {
+        "SCENARIO_NAME": "Beijing_Beihuan",
+        "SUMOCFG": "easy_high_density.sumocfg",
+        "NETFILE": "./networks/easy.net.xml",
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 600,
+        "PHASE_NUMBER": 3,
+        "MOVEMENT_NUMBER": 6, 
+        "CENTER_COORDINATES": (835, 360, 50),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:1, 2:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> Hongkong_YMT
+    "Hongkong_YMT_Test": {
+        "SCENARIO_NAME": "Hongkong_YMT", # 场景所在文件夹
+        "SUMOCFG": "easy.sumocfg", # sumocfg 中包含 route & network 的组合
+        "NETFILE": "./networks/easy.net.xml", # 需要 network 加载地图信息
+        "JUNCTION_NAME": "J1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 3, # Phase 数量
+        "MOVEMENT_NUMBER": 6, 
+        "CENTER_COORDINATES": (172, 201, 60),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:0, 3:1},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> France_Massy
+    "France_Massy_Test": {
+        "SCENARIO_NAME": "France_Massy",
+        "SUMOCFG": "easy.sumocfg",
+        "NETFILE": "./networks/easy.net.xml", # 需要 network 加载地图信息
+        "JUNCTION_NAME": "INT1",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 3,
+        "MOVEMENT_NUMBER": 6, 
+        "CENTER_COORDINATES": (173, 244, 60),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:1, 2:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+    },
+
+    # ===> SouthKorea_Songdo
+    "SouthKorea_Songdo_Test": {
+        "SCENARIO_NAME": "SouthKorea_Songdo",
+        "SUMOCFG": "easy.sumocfg",
+        "NETFILE": "./networks/easy.net.xml", # 需要 network 加载地图信息
+        "JUNCTION_NAME": "J2",
+        "NUM_SECONDS": 500,
+        "PHASE_NUMBER": 4,
+        "MOVEMENT_NUMBER": 12, 
+        "CENTER_COORDINATES": (900, 1641, 100),
+        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0},
+        "ACCIDENTS": None,
+        "SPECIAL_VEHICLES": None
+        },
+
+    # ===> 
     "Hongkong_YMT_NORMAL": {
         # ================== 基础场景参数 ==================
         "SCENARIO_NAME": "Hongkong_YMT", # 场景所在文件夹
@@ -85,20 +253,4 @@ SCENARIO_CONFIGS = {
             },
         ]
     },
-    "SouthKorea_Songdo": {
-        "SCENARIO_NAME": "SouthKorea_Songdo",
-        "SUMOCFG": "songdo_eval",
-        "JUNCTION_NAME": "J2",
-        "PHASE_NUMBER": 4,
-        "CENTER_COORDINATES": (900, 1641, 100),
-        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:3, 2:1, 3:0}
-    },
-    "France_Massy": {
-        "SCENARIO_NAME": "France_Massy",
-        "SUMOCFG": "massy_eval",
-        "JUNCTION_NAME": "INT1",
-        "PHASE_NUMBER": 3,
-        "CENTER_COORDINATES": (173, 244, 100),
-        "SENSOR_INDEX_2_PHASE_INDEX": {0:2, 1:1, 2:0}
-    }
 }
