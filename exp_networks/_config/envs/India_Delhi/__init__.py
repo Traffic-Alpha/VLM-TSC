e6139496526a098f2df83b759bc80d7a11e409bf
wmn7 <18811371255@163.com>
2025-12-19 15:52:24 +0800

    update config

---
+++ exp_networks/_config/envs/India_Delhi/__init__.py
@@ -0,0 +1,7 @@
+'''
+Author: WANG Maonan
+Date: 2025-09-28 15:23:02
+LastEditors: WANG Maonan
+Description: India New Delhi
+LastEditTime: 2025-09-28 15:23:04
+'''
---
+++ exp_networks/_config/envs/SouthKorea_Songdo/__init__.py
@@ -0,0 +1,7 @@
+'''
+Author: WANG Maonan
+Date: 2025-08-04 18:01:44
+LastEditors: WANG Maonan
+Description: Config for SouthKorea Songdo
+LastEditTime: 2025-08-21 15:47:38
+'''
